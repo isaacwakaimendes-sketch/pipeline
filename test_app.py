a4bf213ad20fe8b07f4dd383378eb097cf12e03a
Isaac <isaacwakaimendes@gmail.com>
2026-02-05 11:23:19 +0000

    testee

--- test_app.py
+++ test_app.py
@@ -1,13 +1,4 @@
-# test_app.py
-from app import salvar_texto
+from app import soma
 
-def test_salvar_texto(tmp_path):
-    # Cria um arquivo temporário chamado teste.txt
-    arquivo = tmp_path / "teste.txt"
-    
-    # Chama a função que queremos testar
-    salvar_texto("Olá, mundo!", arquivo)
-    
-    # Lê o conteúdo do arquivo e verifica se está correto
-    conteudo = arquivo.read_text(encoding="utf-8")
-    assert conteudo == "Olá, mundo!"
+def test_soma():
+    assert soma(2, 3) == 5
