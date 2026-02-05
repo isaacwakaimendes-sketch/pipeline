0ab1dcc3a54d735a30f1faf3f3a1d1541f6ccdd2
Isaac <isaacwakaimendes@gmail.com>
2026-02-05 11:18:02 +0000

    teste - texto 2

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import salvar_texto
 def test_salvar_texto(tmp_path):
     arquivo = tmp_path / "teste.txt"
     salvar_texto("Olá, mundo!", arquivo)
-    conteudo = arquivo.read_text()
+    conteudo = arquivo.read_text(encoding="utf-8")
     assert conteudo == "Olá, mundo!"
