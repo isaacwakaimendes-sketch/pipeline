3280971e2c724773c377b779bc0a575a8af02711
Isaac <isaacwakaimendes@gmail.com>
2026-02-05 14:17:53 +0000

    teste.

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from app import criar_tabela, adicionar_usuario, buscar_usuario
 @pytest.fixture
 def db_conn():
     """Cria uma conexão SQLite em memória antes de cada teste e fecha depois"""
-    conn = sqlite3.connect("pipelinebd.db")  
+    conn = sqlite3.connect("basededados.db")  
     criar_tabela(conn)
     yield conn
     conn.close()
