f177896aeae91e337502136c67fb4060be6b12e1
Isaac <isaacwakaimendes@gmail.com>
2026-02-05 15:15:11 +0000

    teste jsonld

--- ver_jsonld.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-caminho = Path("data/utilizadores.jsonld")
-with open(caminho, encoding="utf-8") as f:
-    dados = json.load(f)
-
-print(json.dumps(dados, indent=2, ensure_ascii=False))
